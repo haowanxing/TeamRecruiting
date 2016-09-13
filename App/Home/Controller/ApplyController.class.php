de0fe301518a917d2baa529c4768e4aaafed9c6d
Anthony_box <york_mail@qq.com>
2016-09-13 09:32:24 +0800

    增加社团独立介绍页面

--- App/Home/Controller/ApplyController.class.php
+++ App/Home/Controller/ApplyController.class.php
@@ -1,24 +1,43 @@
 <?php
 namespace Home\Controller;
+
 use Think\Controller;
-class ApplyController extends WebController {
-    public function join($t = 1){
+
+class ApplyController extends WebController
+{
+    public function join($t = 1)
+    {
         $this->departments = M('department')->select();
         $team = D('team')->getInfo($t);
         $this->team_id = $t;
-        $this->index_title = $team['name'].' ';
+        $this->index_title = $team['name'] . ' ';
         $this->apply_description = $team['desc'];
         $this->display();
+    }
+
+    public function intro($tid)
+    {
+        if (!$tid) {
+            $this->error('页面不存在');
+        } else {
+            $team = D('Team')->getInfo($tid);
+            if (!$team) {
+                $this->error('找不到该团队!');
+            }else{
+                $this->assign('team',$team);
+                $this->display();
+            }
+        }
     }
 
     public function doUpdate()
     {
         $data = D('apply')->insertApply();
-        if($data){
-            $retMsg = array('code'=>200,'msg'=>'申请记录已经保存!','info'=>$data);
+        if ($data) {
+            $retMsg = array('code' => 200, 'msg' => '申请记录已经保存!', 'info' => $data);
             $apply_team = D('team')->getInfo($data['tid']);
-            if($apply_team['notice']){
-                $Title = "社团申请通知:".$data['name']."申请加入".$apply_team['name'];
+            if ($apply_team['notice']) {
+                $Title = "社团申请通知:" . $data['name'] . "申请加入" . $apply_team['name'];
                 $Content = <<<CONTENT
 亲爱的【{$apply_team['name']}】社团:<br><br>
 &nbsp;&nbsp;&nbsp;&nbsp;您有一封来自<span style="color:red;">{$data['name']}</span>同学的入社申请,请注意及时处理!
@@ -26,18 +45,20 @@ class ApplyController extends WebController {
 温馨提示:本邮件由创明软件工作室自动发出,请不要直接进行回复此邮件!
 CONTENT;
 
-                SendMail_Sock($apply_team['email'],$Title,$Content,C('WEB_NAME'));
+                SendMail_Sock($apply_team['email'], $Title, $Content, C('WEB_NAME'));
             }
-        }else{
-            $retMsg = array('code'=>400,'msg'=>D("apply")->getError());
+        } else {
+            $retMsg = array('code' => 400, 'msg' => D("apply")->getError());
         }
-        if(IS_AJAX){
+        if (IS_AJAX) {
             $this->ajaxReturn($retMsg);
-        }else{
+        } else {
             $this->result = $retMsg;
         }
     }
-    public function test(){
+
+    public function test()
+    {
 //        import('@.ORG.Mail');
 //        SendMail('york_mail@qq.com','邮件标','邮件正2文','歪酷');
     }
